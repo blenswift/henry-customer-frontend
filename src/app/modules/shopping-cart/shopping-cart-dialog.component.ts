dff743124a652eaf518d133d87f8499508d17c83
sander1991 <>
2023-03-06 20:33:00 +0100

    ExhaustMap

--- src/app/modules/shopping-cart/shopping-cart-dialog.component.ts
+++ src/app/modules/shopping-cart/shopping-cart-dialog.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AngularFireMessaging } from '@angular/fire/compat/messaging';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterModule } from '@angular/router';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { concatMap, exhaustMap, of, Subject, tap } from 'rxjs';
 import { ProductCart } from 'src/app/shared/models/product-cart';
 import { SumOfProductsPipe } from 'src/app/shared/pipes/sum-of-products.pipe';
 import { ShoppingCartService } from 'src/app/shared/services/shopping-cart.service';
@@ -19,7 +20,7 @@ import { CartMainComponent } from './components/cart-main/cart-main.component';
   templateUrl: './shopping-cart-dialog.component.html',
   styleUrls: ['./shopping-cart-dialog.component.scss'],
 })
-export class ShoppingCartDialogComponent {
+export class ShoppingCartDialogComponent implements OnInit {
   shoppingcartService = inject(ShoppingCartService);
   translateService = inject(TranslateService);
   orderService = inject(OrderService);
@@ -27,8 +28,26 @@ export class ShoppingCartDialogComponent {
   angularFireMessaging = inject(AngularFireMessaging);
   paymentType: PaymentType = 'DIGITAL_PAYMENT';
 
+  bezahlenTrigger = new Subject<void>();
+
   public items$ = this.shoppingcartService.items$;
   public tip = 0;
+
+  ngOnInit(): void {
+    this.bezahlenTrigger
+      .pipe(
+        concatMap(() => {
+          const order = this.shoppingcartService.getOrder(this.tip);
+          order.paymentMethod = this.paymentType;
+          order.fcmToken = this.shoppingcartService.fcmToken.value;
+          return of(order);
+        }),
+        exhaustMap(order =>
+          this.orderService.createOrder(sessionStorage.getItem('qrcode')!, order).pipe(tap(data => (window.location.href = data.url)))
+        )
+      )
+      .subscribe();
+  }
 
   removeProduct(product: ProductCart) {
     this.shoppingcartService.remove(product);
@@ -36,14 +55,5 @@ export class ShoppingCartDialogComponent {
 
   paymentTypeChanged(paymentType: PaymentType) {
     this.paymentType = paymentType;
-  }
-
-  navigateToPayment() {
-    const order = this.shoppingcartService.getOrder(this.tip);
-    order.paymentMethod = this.paymentType;
-    order.fcmToken = this.shoppingcartService.fcmToken.value;
-    this.orderService.createOrder(sessionStorage.getItem('qrcode')!, order).subscribe(data => {
-      window.location.href = data.url;
-    });
   }
 }
