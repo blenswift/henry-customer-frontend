614f736ddb3f8436520c29000a97e78d6f9f5bd5
sander1991 <>
2023-08-11 09:12:11 +0200

    neues Design

--- src/app/modules/menu/components/menu-category-carousel/menu-category-carousel.component.ts
+++ src/app/modules/menu/components/menu-category-carousel/menu-category-carousel.component.ts
@@ -1,16 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { TranslateModule } from '@ngx-translate/core';
 import { Category } from 'src/app/shared/models/category';
 import { CategoryComponent } from './category/category.component';
 
 @Component({
   selector: 'oxp-menu-category-carousel',
   standalone: true,
-  imports: [CommonModule, CategoryComponent, MatDividerModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  imports: [
+    CommonModule,
+    CategoryComponent,
+    MatDividerModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    TranslateModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatBadgeModule,
+  ],
   templateUrl: './menu-category-carousel.component.html',
   styleUrls: ['./menu-category-carousel.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -28,10 +44,14 @@ export class MenuCategoryCarouselComponent {
   public get categories(): Category[] | null {
     return this._categories;
   }
+  @Input()
+  public filterCtrl!: FormControl;
 
   @Output() categoryClicked = new EventEmitter<Category>();
 
   categorySelected: Category | null = null;
+
+  showSearch = false;
 
   clicked(category: Category) {
     if (this.categorySelected) {
@@ -41,5 +61,12 @@ export class MenuCategoryCarouselComponent {
     this.categorySelected.selected = true;
     this.categoryClicked.emit(category);
     this.categories = structuredClone(this.categories);
+  }
+
+  focus() {
+    // setTimeout(() => {
+    // this will make the execution after the above boolean has changed
+    document.getElementById('filterCtrl')?.focus();
+    // }, 0);
   }
 }
