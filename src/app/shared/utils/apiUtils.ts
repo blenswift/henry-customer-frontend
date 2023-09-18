cb01ea18e33ca45d1ec5dfd46813b0ecf726f849
sander1991 <>
2023-09-18 10:37:07 +0200

    api url

--- src/app/shared/utils/apiUtils.ts
+++ src/app/shared/utils/apiUtils.ts
@@ -1,7 +1,11 @@
 export function getApiUrl(): string {
-  const currentUrl = window.location.origin;
   if (window.location.host.includes('localhost')) {
     return 'https://api.dev.henryapp.de/v1';
+  } else if (window.location.host.includes('dev')) {
+    return 'https://api.dev.henryapp.de/v1';
+  } else if (window.location.host.includes('staging')) {
+    return 'https://api.staging.henryapp.de/v1';
+  } else {
+    return 'https://api.henryapp.de/v1';
   }
-  return currentUrl + '/v1';
 }
