3ad0434de0669359de07aad6a54a0d782e37a21d
orderxpay <122056820+orderxpay@users.noreply.github.com>
2023-10-24 01:19:01 +0200

    Update apiUtils.ts
    added demo api

--- src/app/shared/utils/apiUtils.ts
+++ src/app/shared/utils/apiUtils.ts
@@ -3,6 +3,8 @@ export function getApiUrl(): string {
     return 'https://api.dev.henryapp.de/v1';
   } else if (window.location.host.includes('dev')) {
     return 'https://api.dev.henryapp.de/v1';
+  } else if (window.location.host.includes('demo')) {
+    return 'https://api.demo.henryapp.de/v1';
   } else if (window.location.host.includes('staging')) {
     return 'https://api.staging.henryapp.de/v1';
   } else {
