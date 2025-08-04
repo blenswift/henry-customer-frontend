736075c518a6811b2c66b6ea77e105e9c4a36f60
florian <florian.westmeier@gmx.de>
2025-08-04 11:49:20 +0200

    new

--- src/app/shared/utils/apiUtils.ts
+++ src/app/shared/utils/apiUtils.ts
@@ -1,4 +1,8 @@
 export function getApiUrl(): string {
-  return (window as any).API_URL + '/v1' || '';
+  if (window.location.host.includes('demo')) {
+    return 'https://demo-api.henryapp.de/v1';
+  } else {
+    return 'https://api.henryapp.de/v1';
+  }
 }
 
