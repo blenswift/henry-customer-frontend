330b7d845918217b7f8f8340158401f0f2c75bba
blenswift <florian.westmeier@blenswift-technology.de>
2025-08-04 02:10:26 +0200

    Update apiUtils.ts

--- src/app/shared/utils/apiUtils.ts
+++ src/app/shared/utils/apiUtils.ts
@@ -1,13 +1,4 @@
 export function getApiUrl(): string {
-  if (window.location.host.includes('localhost')) {
-    return 'https://api.dev.henryapp.de/v1';
-  } else if (window.location.host.includes('dev')) {
-    return 'https://api.dev.henryapp.de/v1';
-  } else if (window.location.host.includes('demo')) {
-    return 'https://api.demo.henryapp.de/v1';
-  } else if (window.location.host.includes('staging')) {
-    return 'https://api.staging.henryapp.de/v1';
-  } else {
-    return 'https://api.henryapp.de/v1';
-  }
+  return (window as any).API_URL + '/v1' || '';
 }
+
